cd0d9a9d3d6270e850e88bb89031fa8c06f86e7b
GautamSPhadke <gphadke@usc.edu>
2019-11-20 00:29:20 -0800

    added misc.old vgg py

---
+++ misc/old_vgg.py
@@ -0,0 +1,16 @@
+import torch.utils.data as data
+from PIL import Image
+import torch
+import numpy as np
+import h5py
+import json
+import pdb
+import random
+from misc.utils import repackage_hidden, clip_gradient, adjust_learning_rate, decode_txt
+
+file = '/home/smit/PycharmProjects/visDial_CPU/visDial.pytorch/script/data/vdl_img_vgg_demo.h5'
+f = h5py.File(file)
+imgs = f['images_train']
+
+def old_vgg_16(idx):
+    return torch.from_numpy(imgs[idx])
